56a3b020ea6ac6994bf5a551081c4b30ab32b046
DELL <DELL@DESKTOP-U0T0RH6>
2023-04-14 23:13:46 -0700

    this is the class

---
+++ src/newPackage/NEWClass.java
@@ -0,0 +1,18 @@
+package newPackage;
+
+public class NEWClass {
+
+	public static void main(String[] args) {
+		String name = "aviansh";
+		if(name =="nasina") {
+			System.out.println("nasina Avinash");		
+			}
+		else { 
+			System.out.println("avinash");
+		}
+			
+		}
+
+	}
+
+
